05a6fb86b97b9020cf9662ba627c11a1c743ca43
KANEEZ-FATIMA18 <k.fatima182005@gmail.com>
2024-11-16 14:49:09 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 'use client'
-import { useState } from "react"
+import React, { useState } from 'react';
 
-export default function usestate() {
+export default function UseState() {
   const[count,setCount]= useState(0)
 
   return(
     <div className="flex flex-col justify-center items-center min-h-screen bg-lime-500">
-    <div className="text-white font-bold text-2xl mb-7"> {count}</div>
+    <div className="text-white font-bold text-2xl mb-7"> The Count is {count}</div>
     <button className="text-xl font-semibold  bg-white px-5 py-2 border-[3px] border-black rounded-lg" onClick={() =>{setCount(count+1)}}>Hello peeps please click me</button>
     </div>
 
